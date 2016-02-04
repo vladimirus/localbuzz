0aac7a6a3495a75f95d756500b9341b421ffac56
Vladimirus <vladimirus@gmail.com>
2016-02-04 16:58:45 +0000

    optimised imports

--- app/models/Article.scala
+++ app/models/Article.scala
@@ -8,13 +8,13 @@ import play.api.Logger
 
 object Article {
   case object Update
-  def props(chat: ActorRef): Props = Props(new Article(chat))
+  def props(chat: ActorRef) = Props(new Article(chat))
 }
 
 class Article(chat: ActorRef) extends Actor {
   import Article._
 
-  override def receive: Receive = {
+  override def receive = {
     case Update =>
       chat ! latestArticles
   }
