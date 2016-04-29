9800573c36ad7ce6f5683fdb4472c134b2d9adec
Vladimirus <vladimirus@gmail.com>
2016-04-29 12:09:50 +0100

    updated js

--- public/javascripts/hello.js
+++ public/javascripts/hello.js
@@ -1,8 +1,10 @@
 angular.module("ChatApp", []).controller("ChatController", function($scope){
   // connect to websockets endpoint of our server
-  var ws = new WebSocket("ws://" + document.location.host + ":9000/socket");
+  var host = window.document.location.host.replace(/:.*/, '');
+  var port = window.document.location.port;
+  var ws = new WebSocket('ws://'+ host+':'+ port +'/socket');
 
-  var chat = this;
+    var chat = this;
   chat.messages = [];
 
   ws.onmessage = function(msg) {
